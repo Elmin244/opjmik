dff94056d1c789409fca47e48679bb81eebd4737
Roza Abdullayeva <tu7t6q96t@aztu.local>
2022-03-11 15:01:55 +0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int[] array = { 4, 5, 6, 7 };
+            int EvenNumber = 0;
+            PrintArrayItem( array );
+        }
+        static void PrintArrayItem(int[] array)
+        {
+
+            foreach (var item in array)
+            {
+                if (item % 2 == 0 )
+                {
+                    Console.WriteLine(item);
+                }
+               
+            }
+        }
+           
+    }
+}
